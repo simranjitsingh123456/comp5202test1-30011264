7031652f126175e13ee051ae27846d2537216197
Simranjit Singh <30011264@student.toiohomai.ac.nz>
2018-08-30 14:50:22 +1200

    msg

---
+++ consoleapp odd or even/Programax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two whole numbers n and x:");
+            int n = int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
+            int Factorial = 1;
+            double sum = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                Factorial *= i;
+                sum += (Factorial / Math.Pow(x, i));
+            }
+            Console.WriteLine("{0:F5}", sum);
+        }
+    }
+}
---
+++ consoleapp odd or even/consoleapp odd or even/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleapp_odd_or_even
+{
+    class Program
+    {
+        public static int Number1 { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a numder to find odd or even");
+            Number1 = int.Parse(Console.ReadLine());
+            if (Number1 % 1 == 0)
+            {
+                Console.WriteLine(" your even numder");
+
+
+            }
+            else (Number1 % 1 == 0)
+            {
+                Console.WriteLine(" your odd number");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
